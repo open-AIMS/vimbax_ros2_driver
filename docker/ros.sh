1226e9e5a93e9d40df445e7234934cb7208933e8
Greg Coleman <g.coleman@aims.gov.au>
2025-04-02 18:42:02 +1000

    A couple of small mistakes

--- docker/ros.sh
+++ docker/ros.sh
@@ -2,5 +2,6 @@
 source /opt/ros/humble/setup.bash
 source /root/ros2_ws/install/local_setup.bash
 source /root/ros2_ws/install/setup.bash
-
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+source /etc/profile.d/VimbaX_GenTL_Path_64bit.sh
 
