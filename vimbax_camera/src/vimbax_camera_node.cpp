8b26ebc443fa192781adf580f61ec5004e4fdd58
gcoleman72 <g.coleman@aims.gov.au>
2025-05-06 11:34:22 +1000

    no queue

--- vimbax_camera/src/vimbax_camera_node.cpp
+++ vimbax_camera/src/vimbax_camera_node.cpp
@@ -352,7 +352,7 @@ bool VimbaXCameraNode::initialize_publisher()
   RCLCPP_INFO(get_logger(), "Initializing publisher ...");
 
   auto qos = rmw_qos_profile_default;
-  qos.depth = 10;
+  qos.depth = 1;
 
   camera_publisher_ = image_transport::create_camera_publisher(node_.get(), "image_raw", qos);
 
